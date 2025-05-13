b15d6692553b9c2c08f7c9bc978b13933f0e9bd1
Kevin Denver <1027637+ToastShaman@users.noreply.github.com>
2025-05-13 10:15:28 +0100

    Update health-check

--- 05-2025/juice-shop/lib/juice-shop-stack.ts
+++ 05-2025/juice-shop/lib/juice-shop-stack.ts
@@ -24,6 +24,9 @@ export class JuiceShopStack extends cdk.Stack {
       taskImageOptions: {
         image: ecs.ContainerImage.fromRegistry('bkimminich/juice-shop'),
         containerPort: 3000,
+        environment: {
+          'NODE_ENV': 'ctf',
+        }
       }
     });
 
@@ -33,6 +36,12 @@ export class JuiceShopStack extends cdk.Stack {
       memoryLimitMiB: 512,
       desiredCount: 1,
       minHealthyPercent: 100,
+      healthCheck: {
+        command: ['CMD-SHELL', 'curl -s --connect-timeout 5 http://localhost:8000/ > /dev/null 2>&1 || exit 1'],
+        interval: cdk.Duration.seconds(30),
+        timeout: cdk.Duration.seconds(5),
+        retries: 5,
+      },
       publicLoadBalancer: true,
       taskImageOptions: {
         image: ecs.ContainerImage.fromRegistry('ctfd/ctfd'),
